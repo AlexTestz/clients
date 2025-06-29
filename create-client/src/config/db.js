f73ee917cee61fafea58b15cb1da04098be7eec7
AlexMorales <admoralesp@uce.edu.ec>
2025-06-29 18:32:40 -0500

    new

--- create-client/src/config/db.js
+++ create-client/src/config/db.js
@@ -6,14 +6,17 @@ if (process.env.NODE_ENV !== 'ci') {
   dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 }
 
+const useSSL = true;  // fuerza usar SSL, porque AWS RDS lo requiere
+
+console.log(`Conectando a PostgreSQL con SSL: ${useSSL}`);
+
 const pool = new Pool({
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
-  ssl: useSSL ? {rejectUnauthorized: false } : false,// Esto es necesario si estás usando un certificado autofirmado
-  }
+  ssl: useSSL ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
