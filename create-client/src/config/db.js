311303a0473afefbdf5c8f9b774bd451da0dd883
AlexMorales <admoralesp@uce.edu.ec>
2025-06-29 18:20:45 -0500

    feat: :fire: authorized ssl

--- create-client/src/config/db.js
+++ create-client/src/config/db.js
@@ -12,6 +12,9 @@ const pool = new Pool({
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
+  ssl:{
+    rejectUnauthorized: false // Esto es necesario si estás usando un certificado autofirmado
+  }
 });
 
 module.exports = pool;
