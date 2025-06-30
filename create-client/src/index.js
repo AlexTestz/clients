760f3b8edb833a125a6a5414c757d5cdd42d6c23
AlexMorales <admoralesp@uce.edu.ec>
2025-06-29 23:32:52 -0500

    action 3

--- create-client/src/index.js
+++ create-client/src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config(); // cargar variables de entorno desde .env
 const express = require('express');
 const cors = require('cors'); 
 const app = express();
